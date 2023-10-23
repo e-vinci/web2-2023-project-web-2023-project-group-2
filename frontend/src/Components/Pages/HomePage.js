9d71c8c2cfcab09c0c68a120a12c6188ca2f31bf
Ibrahim-Ayachi-vinci <128418933+Ibrahim-Ayachi-vinci@users.noreply.github.com>
2023-10-23 12:42:35 +0200

    Début du projet

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,8 @@
 const HomePage = () => {
-  const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  const main = document.querySelector('main')
+  const text = `<img src = "../../img/virus-4986015_12801.png">`;
+
+  main.innerHTML = text;
 };
 
 export default HomePage;
