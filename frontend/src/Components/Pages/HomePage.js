91c2dfab0de55dd3c2144ae96d0cd10b621e20d0
Nicolae Pastrama <nicolae.pastrama@student.vinci.be>
2023-10-24 15:24:27 +0200

    removed homepage image

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,8 +1,6 @@
-import imageFond from '../../img/world-36479_1280.png';
-
 const HomePage = () => {
   const main = document.querySelector('main');
-  const text = `<div class = "text-center"> <img class ="img-fluid" src=${imageFond}>`;
+  const text = ``;
   main.innerHTML = text;
 
 };
