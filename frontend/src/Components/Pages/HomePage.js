37f9ae0b2cbeb4c4948df92ef3ebb1ff9feed6e6
Ibrahim-Ayachi-vinci <128418933+Ibrahim-Ayachi-vinci@users.noreply.github.com>
2023-10-24 14:13:45 +0200

    ajustement image

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -2,7 +2,7 @@ import imageFond from '../../img/world-36479_1280.png';
 
 const HomePage = () => {
   const main = document.querySelector('main');
-  const text = `<div> <img src=${imageFond}>`;
+  const text = `<div> <img class ="img-fluid" src=${imageFond}>`;
   main.innerHTML = text;
 
 };
