1613782b729a5263858cc7235bd051caeb293fc5
hamadi-gaddih-vinci <hamadi.gaddih@student.vinci.beC:Web2>
2023-11-07 14:18:03 +0100

    Navbar changer

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,45 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import { Navbar as BootstrapNavbar } from 'bootstrap';
+import image from '../../img/logoNav.png';
 
 /**
  * Render the Navbar which is styled by using Bootstrap
  * Each item in the Navbar is tightly coupled with the Router configuration :
  * - the URI associated to a page shall be given in the attribute "data-uri" of the Navbar
- * - the router will show the Page associated to this URI when the user click on a nav-link
+ * - the router will show the Page associated to this URI when the user clicks on a nav-link
  */
 
 const Navbar = () => {
   const navbarWrapper = document.querySelector('#navbarWrapper');
   const navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light" style = "background-color: #74F071">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Add your brand here</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/game">Game</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/new">New Page</a>
-              </li>                        
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="${image}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+          CovidClicker
+        </a>
+        <button class="btn btn-primary ml-auto">Se connecter</button>
+      </div>
+    </nav>
   `;
   navbarWrapper.innerHTML = navbar;
 };
