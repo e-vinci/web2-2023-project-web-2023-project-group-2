1ace33480e37a9c9cd45fb9ee6d24bdc134e55ef
Ibrahim-Ayachi-vinci <128418933+Ibrahim-Ayachi-vinci@users.noreply.github.com>
2023-11-14 15:29:50 +0100

    Ajout du début de la route clicker

--- api/routes/clicker.js
+++ api/routes/clicker.js
@@ -2,4 +2,8 @@ const express = require('express');
 // const { } = require('../models/clicker');
 const router = express.Router();
 
-router.get('/', (req, res) => res.json());
+router.get('/registerScore', async (req, res) => {
+  const iduser = req?.body?.idUser >= 1 ? req.body.idUser : undefined;
+
+  if (!iduser) return res.sendStatus(400);
+});
