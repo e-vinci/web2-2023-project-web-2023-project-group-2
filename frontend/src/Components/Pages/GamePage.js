938fd1e40bcc4e98fc43f130d10d6ccc2194792d
Erismann <matteo.erismann@student.vinci.be>
2023-11-22 22:18:03 +0100

    clicker animation

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -1,36 +1,48 @@
-import Phaser from 'phaser';
-import GameScene from '../Game/GameScene';
-
-let game;
+import anime from 'animejs/lib/anime.es'
+import covidImage from '../../img/covid-19-5551910_1280.png'
 
 const GamePage = () => {
-  const phaserGame = `
-<div id="gameDiv" class="d-flex justify-content-center my-3">
-</div>`;
 
   const main = document.querySelector('main');
-  main.innerHTML = phaserGame;
+  
+  const text = ` 
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+  <div class="covidContainer">
+    <div>
+      <img src="${covidImage}" class="covidClick" alt="PLAY" id="covidImg"> 
+    </div>
+  </div>
+  </div>
+  `;
+  main.innerHTML = text;
 
-  const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 300 },
-        debug: false,
-      },
-    },
-    scene: [GameScene],
-    //  parent DOM element into which the canvas created by the renderer will be injected.
-    parent: 'gameDiv',
-  };
+  const covidClick = document.querySelector('#covidImg')
 
-  // there could be issues when a game was quit (events no longer working)
-  // therefore destroy any started game prior to recreate it
-  if (game) game.destroy(true);
-  game = new Phaser.Game(config);
+  covidClick.addEventListener('click', clickOnCovid)
+
+  function clickOnCovid() {
+    console.log('clické')
+
+    anime({
+      targets : '.covidClick',
+      scale : 1.2,
+      duration : 200,
+      easing : 'easeInOutQuad',
+      complete : animeEnd
+    })
+    
+  }
+
+  function animeEnd() {
+    anime({
+      targets : '.covidClick',
+      scale : 1.0,
+      duration : 100,
+      easing : 'easeInOutQuad',
+    })
+  }
+
+  
 };
 
 export default GamePage;
