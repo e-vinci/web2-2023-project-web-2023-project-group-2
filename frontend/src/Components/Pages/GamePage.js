6370cc27aec596dd29e84fafeadfce7d98d49784
Erismann <matteo.erismann@student.vinci.be>
2023-11-24 15:55:34 +0100

    background change + popvalue start

--- frontend/src/Components/Pages/GamePage.js
+++ frontend/src/Components/Pages/GamePage.js
@@ -2,14 +2,22 @@ import anime from 'animejs/lib/anime.es'
 import covidImage from '../../img/covid-19-5551910_1280.png'
 
 const GamePage = () => {
+  const score = 0;
+  // const clickValue = 1;
 
   const main = document.querySelector('main');
+
+  main.className = 'homepage_body'
   
   const text = ` 
-  <div class="container d-flex justify-content-center align-items-center vh-100">
+  <div class="container d-flex justify-content-evenly align-items-center vh-100 flex-column">
+  <div class="alert alert-dark" role="alert">
+  ${score}
+  </div>
   <div class="covidContainer">
+    <div id="clickFeedback" class="click-feedback"></div>
     <div>
-      <img src="${covidImage}" class="covidClick" alt="PLAY" id="covidImg"> 
+      <img src="${covidImage}" class="covidClick" alt="PLAY" id="covidImg" width="350" height="350"> 
     </div>
   </div>
   </div>
@@ -19,10 +27,9 @@ const GamePage = () => {
   const covidClick = document.querySelector('#covidImg')
 
   covidClick.addEventListener('click', clickOnCovid)
+  // covidClick.addEventListener('clcik', popValueAnimation)
 
   function clickOnCovid() {
-    console.log('clické')
-
     anime({
       targets : '.covidClick',
       scale : 1.2,
@@ -41,6 +48,10 @@ const GamePage = () => {
       easing : 'easeInOutQuad',
     })
   }
+
+  /* function popValueAnimation(e) {
+    
+  } */
 
   
 };
