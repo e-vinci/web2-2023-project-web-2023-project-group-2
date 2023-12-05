70c568786a930feecb269c5624e1fa7f57d280a5
Erismann <matteo.erismann@student.vinci.be>
2023-12-05 13:57:08 +0100

    leaderboard start

--- frontend/src/Components/Pages/Leaderboard.js
+++ frontend/src/Components/Pages/Leaderboard.js
@@ -1,20 +1,36 @@
-const Leaderboard = () => {
+const getUsers = async () => {
+  try{
+    const response = await fetch('/api/leaderBoard');
+    if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+    const users = await response.json;
+    return users;
+
+
+  }catch (err){
+    console.error('LeaderBoard::error : ', err)
+  }
+}
+
+const Leaderboard = async () => {
+
   const main = document.querySelector('main');
+  const users = await getUsers();
+
   const text = `
-    <table class="table">
-     <thead>
-       <tr>
-         <th scope="col">#</th>
-         <th scope="col">Pseudo</th>
-         <th scope="col">score</th>
-       </tr>
-     </thead>
-     <tbody>
-     
-     </tbody>
-    </table>
-    `;
-  main.innerHTML = text;
-};
+  <table class="table">
+   <thead>
+     <tr>
+       <th scope="col">#</th>
+       <th scope="col">Pseudo</th>
+       <th scope="col">score</th>
+     </tr>
+   </thead>
+   <tbody>
+   
+   </tbody>
+  </table>
+  `;
+main.innerHTML = text;
+}
 
 export default Leaderboard;
