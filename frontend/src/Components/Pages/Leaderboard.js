da38f6ba8cab66beb3dc90a4493591bd8592de6d
Erismann <matteo.erismann@student.vinci.be>
2023-12-05 15:25:17 +0100

    leaderboard in progress

--- frontend/src/Components/Pages/Leaderboard.js
+++ frontend/src/Components/Pages/Leaderboard.js
@@ -1,13 +1,16 @@
-const getUsers = async () => {
+async function getUsers() {
   try{
+
     const response = await fetch('/api/leaderBoard');
     if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-    const users = await response.json;
+    
+    const users = await response.json();
     return users;
 
 
   }catch (err){
-    console.error('LeaderBoard::error : ', err)
+    console.error('LeaderBoard::error : ',err)
+    throw err;
   }
 }
 
@@ -26,11 +29,22 @@ const Leaderboard = async () => {
      </tr>
    </thead>
    <tbody>
-   
+   ${tableLines}
    </tbody>
   </table>
   `;
 main.innerHTML = text;
-}
 
+function tableLines(){
+  let lines = '';
+
+  users?.forEach(user => {
+    lines += `<tr>
+    <td>${user.findIndex()}
+    <td>${user.username}</td>
+    <td>${user.nbClick}`
+  });
+  return lines
+}
+}
 export default Leaderboard;
