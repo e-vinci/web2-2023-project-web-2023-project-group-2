c772c70200db46a0983eebecd9eb547cb61e77b6
Ibrahim Ayachi <ibrahim.ayachi@student.vinci.be>
2023-12-14 00:53:37 +0100

    Fix

--- api/models/userUpgrades.js
+++ api/models/userUpgrades.js
@@ -95,6 +95,8 @@ async function updateCostUpgrade(id, idU) {
         userUpdate[foundIndexUpgrade].cost = Math.ceil(userUpdate[foundIndexUpgrade].cost * 1.05);
       } else if (upgrade.upgradeClickerValue === 2) {
         userUpdate[foundIndexUpgrade].cost = Math.ceil(userUpdate[foundIndexUpgrade].cost * 5);
+      }else if (upgrade.upgradeClickValue === 100) {
+        userUpdate[foundIndexUpgrade].cost = Math.ceil(userUpdate[foundIndexUpgrade].cost * 1.1);
       }
     }
 
